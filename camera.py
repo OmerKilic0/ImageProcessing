fb29eacc352f510effdd5dcc011472f0eb2c5aa6
Omer <busenur44@gmail.com>
2024-07-23 15:44:46 +0300

    OmerCagriBeyza

--- camera.py
+++ camera.py
@@ -8,10 +8,19 @@ while(True):
     ret , frame = cap.read()
 
     cv2.imshow('Frame', frame)
-    
+
+    saveKey = 0xFF
+
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    video = cv2.VideoWriter('output.mp4', fourcc, 30, (640, 640))
+
+    if(saveKey == ord('q')):
+        video.write(frame)
+
     k = cv2.waitKey(25)  & 0xFF
     if k == 27:
         cap.release()
+        video.release()
         cv2.destroyAllWindows()
         break
 
