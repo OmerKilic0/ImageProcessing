769367f45f8baef52b5edfb3a64289090698603e
Omer <busenur44@gmail.com>
2024-07-23 15:32:20 +0300

    Cagri

--- camera.py
+++ camera.py
@@ -3,11 +3,17 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-if(cap.isOpened() == False):
-    print("Cap is not opened")
 
-while(cap.isopened()):
+while(True):
     ret , frame = cap.read()
 
     cv2.imshow('Frame', frame)
     
+    k = cv2.waitKey(25)  & 0xFF
+    if k == 27:
+        cap.release()
+        cv2.destroyAllWindows()
+        break
+
+
+
