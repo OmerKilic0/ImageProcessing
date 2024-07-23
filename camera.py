328cab9a51db50b944ddde93141c81378788ccd5
Omer <busenur44@gmail.com>
2024-07-23 15:24:09 +0300

    Beyza

--- camera.py
+++ camera.py
@@ -1,3 +1,13 @@
 import cv2
 import numpy as np
 
+cap = cv2.VideoCapture(0)
+
+if(cap.isOpened() == False):
+    print("Cap is not opened")
+
+while(cap.isopened()):
+    ret , frame = cap.read()
+
+    cv2.imshow('Frame', frame)
+    
